b09aa74ad4c6c2059bf16b3ee3e2f6d7d0129cbc
Sergio Kulyk <sergiokulyk98@gmail.com>
2018-03-31 21:46:10 +0300

    feat: create click listener

---
+++ app/src/main/java/com/example/android/miwok/NumberClickListener.java
@@ -0,0 +1,16 @@
+package com.example.android.miwok;
+
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by SergeyKulyk on 31.03.2018.
+ */
+
+public class NumberClickListener implements View.OnClickListener {
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(v.getContext(), "Open the list of numbers...", Toast.LENGTH_SHORT).show();
+    }
+}
