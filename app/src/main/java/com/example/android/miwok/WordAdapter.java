27cdd0cbc2ad923c73e40cfad2d425035e6103c6
Sergio Kulyk <sergiokulyk98@gmail.com>
2018-04-03 16:21:40 +0300

    style: set background color for each category items

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,9 +20,10 @@ import java.util.ArrayList;
  */
 
 public class WordAdapter extends ArrayAdapter<Word> {
-
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int mColorResourceId;
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         super(context, 0, words);
+        this.mColorResourceId = colorResourceId;
     }
 
     @NonNull
@@ -49,6 +52,10 @@ public class WordAdapter extends ArrayAdapter<Word> {
         else {
             imageView.setVisibility(View.GONE);
         }
+
+        LinearLayout text_container = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        text_container.setBackgroundColor(color);
 
         return listItemView;
     }
