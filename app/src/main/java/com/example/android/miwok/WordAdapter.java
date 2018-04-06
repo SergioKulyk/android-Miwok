10c81c33fd69ac3bbeeacade41cdc7e5c7199322
Sergio Kulyk <sergiokulyk98@gmail.com>
2018-04-06 21:48:28 +0300

    refactor: delete unnecessary library

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -2,7 +2,6 @@ package com.example.android.miwok;
 
 import android.app.Activity;
 
-import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
